f5d2d8dfbb0f63498d54e1c7c4ef4fb6f2f2a2ac
Dan Jordan <daniel.jordan@mindera.com>
2018-11-02 11:10:23 +0000

    Improve code formatting

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 'use strict';
 
-const path = require('path')
+const path = require('path');
 const { app, BrowserWindow, shell, Tray } = require('electron');
 
 app.on('ready', () => {
-  
   if (process.platform === 'darwin') {
     app.dock.hide();
   }
 
-  var browserWindow = new BrowserWindow({
+  const browserWindow = new BrowserWindow({
     width: 400,
     height: 600,
     resizable: false,
@@ -22,12 +21,15 @@ app.on('ready', () => {
     browserWindow.hide();
   });
 
-  browserWindow.webContents.on('did-get-redirect-request', (event, oldURL, newURL, isMainFrame) => {
-    if (isMainFrame) {
-      setTimeout(() => browserWindow.loadURL(newURL), 100);
-      event.preventDefault();
+  browserWindow.webContents.on(
+    'did-get-redirect-request',
+    (event, oldURL, newURL, isMainFrame) => {
+      if (isMainFrame) {
+        setTimeout(() => browserWindow.loadURL(newURL), 100);
+        event.preventDefault();
+      }
     }
-  });
+  );
 
   browserWindow.webContents.on('new-window', (event, url) => {
     shell.openExternal(url);
@@ -39,26 +41,19 @@ app.on('ready', () => {
   const iconName = 'images/IconTemplate.png';
   const iconPath = path.join(__dirname, iconName);
 
-  var tray = new Tray(iconPath);
-  
+  const tray = new Tray(iconPath);
+
   tray.setToolTip('Twitter');
 
   tray.on('click', () => {
     const trayPos = tray.getBounds();
     const windowPos = browserWindow.getBounds();
-    let x, y = 0;
 
-    if (process.platform == 'darwin') {
-      x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2));
-      y = Math.round(trayPos.y + trayPos.height);
-    } else {
-      x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2));
-      y = Math.round(trayPos.y + trayPos.height * 10);
-    }
-  
+    const x = Math.round(trayPos.x + trayPos.width / 2 - windowPos.width / 2);
+    const y = Math.round(trayPos.y + trayPos.height);
+
     browserWindow.setPosition(x, y, false);
     browserWindow.isVisible() ? browserWindow.hide() : browserWindow.show();
     browserWindow.focus();
   });
-
 });
